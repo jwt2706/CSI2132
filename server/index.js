bd5434ce41eed68776c9bab7b5d512922fddebea
Sacha Arseneault <sacha.arseneault@gmail.com>
2025-03-04 19:55:11 -0500

    boilerplate backend and sql

--- server/index.js
+++ server/index.js
@@ -1,6 +1,20 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const pool = require("./db");
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/hotels", async (req, res) => {
+  try {
+    const allHotels = await pool.query("SELECT * FROM Hotels");
+    res.json(Hotels.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 app.listen(5000, () => {
-	console.log("server has started on port 5000");
+  console.log("server has started on port 5000");
 });
