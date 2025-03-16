45ffc0bf4d257f773d08d99f51d85fedb934e5c7
Sacha Arseneault <sacha.arseneault@gmail.com>
2025-03-16 17:19:14 -0400

    temp: get rooms availabilities (not fully working)

--- server/index.js
+++ server/index.js
@@ -19,6 +19,35 @@ app.get("/:table", async (req, res) => {
   }
 });
 
+// check availability of rooms
+app.get("/availability/rooms", async (req, res) => {
+  try {
+    const { start_date, end_date, capacity, area, hotel_chain_name, hotel_category, num_rooms, price_min, price_max } = req.body;
+    const query = {
+      name: "fetch-rooms-available",
+      text: `WITH date_series AS (
+                SELECT generate_series($1::date, $2::date, '1 day'::interval) AS date
+              )
+              SELECT h.hotel_name, r.room_num, r.capacity, hc.hotel_chain_name, h.category, h.area, r.price
+              FROM rooms r
+              JOIN hotels h ON r.hotel_id = h.id
+              JOIN hotelchains hc ON hc.id = h.hotel_chain_id
+              JOIN roomavailabledates a ON r.hotel_id = a.hotel_id AND r.room_num = a.room_num
+              JOIN date_series ds ON ds.date = a.room_available_date
+              WHERE r.capacity >= $3 AND h.area = $4 AND hc.hotel_chain_name = $5 AND h.category = $6
+              GROUP BY h.hotel_name, r.room_num, r.capacity, hc.hotel_chain_name, h.category, h.area, r.price
+              HAVING COUNT(DISTINCT a.room_available_date) = (SELECT COUNT(*) FROM date_series) AND COUNT(r.room_num) >= $7 AND r.price BETWEEN $8 AND $9`,
+      values: [start_date, end_date, capacity, area, hotel_chain_name, hotel_category, num_rooms, price_min, price_max],
+    };
+    const rooms = await pool.query(query);
+    console.log(rooms);
+    res.json(rooms.rows);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
 // create room booking
 app.put("/hotels/:hotel_id/rooms/:room_num/book", async (req, res) => {
   try {
@@ -58,14 +87,7 @@ app.post("/customers", async (req, res) => {
       name: "register-customer",
       text: `INSERT INTO customers (government_id_type, government_id, first_name, last_name, street_number, street_name, registration_date) 
             VALUES ($1, $2, $3, $4, $5, $6, CURRENT_TIMESTAMP) RETURNING *`,
-      values: [
-        req.body.government_id_type,
-        req.body.government_id,
-        req.body.first_name,
-        req.body.last_name,
-        req.body.street_number,
-        req.body.street_name,
-      ],
+      values: [req.body.government_id_type, req.body.government_id, req.body.first_name, req.body.last_name, req.body.street_number, req.body.street_name],
     };
 
     const post_response = await pool.query(query);
