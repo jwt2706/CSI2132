51058043f55faaf309e0dcd1bc8368acc5b43e07
Sacha Arseneault <sacha.arseneault@gmail.com>
2025-03-18 20:35:27 -0400

    feat: add idx on hotels and rooms wip

--- server/indexes.sql
+++ server/indexes.sql
@@ -2,3 +2,14 @@ DROP INDEX IF EXISTS idx_room_availability_date_range;
 
 CREATE INDEX idx_room_availability_date_range 
 ON RoomAvailableDates (room_available_date, hotel_id, room_num);
+
+DROP INDEX IF EXISTS idx_hotels;
+
+CREATE INDEX idx_rooms 
+ON Hotels (id, nom_rooms, category, hotel_name);
+
+
+DROP INDEX IF EXISTS idx_rooms;
+
+CREATE INDEX idx_rooms 
+ON Rooms (hotel_id, room_num, capacity);
