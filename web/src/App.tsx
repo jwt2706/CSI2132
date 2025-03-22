aaf35422c1a7c8e4ef0a107d67a7809e429b7202
jwt2706 <jwt2home@gmail.com>
2025-03-22 01:32:25 -0400

    add view selector

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,25 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import GetTables from "./components/GetTables";
 import GetAvailableRooms from "./components/GetAvailableRooms";
 
 function App() {
+  const [view, setView] = useState("customer"); // customer view is the default
+
   return (
     <div className="bg-gray-900 text-white min-h-screen w-full m-0 p-0">
-      <Header />
+      <Header view={view} setView={setView} />
       <main className="p-4">
-        <GetAvailableRooms />
-        <br />
-        <GetTables />
+        {view === "customer" && (
+          <>
+            <GetAvailableRooms />
+          </>
+        )}
+        {view === "employee" && (
+          <>
+            <GetTables />
+          </>
+        )}
       </main>
     </div>
   );
--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,11 +1,35 @@
 import React from "react";
 import logo from "/logo.png";
 
-const Header = () => {
+const Header = ({ view, setView }) => {
   return (
-    <header className="w-full flex items-center p-4 bg-gray-800 text-white">
-      <img src={logo} alt="EHotelDB Logo" className="h-12 mr-4" />
-      <h1 className="text-xl">EHotelDB</h1>
+    <header className="w-full flex items-center justify-between p-4 bg-gray-800 text-white">
+      <div className="flex items-center">
+        <img src={logo} alt="EHotelDB Logo" className="h-12 mr-4" />
+        <h1 className="text-xl">EHotelDB</h1>
+      </div>
+      <div className="flex space-x-4">
+        <button
+          className={`px-4 py-2 rounded-md font-semibold ${
+            view === "customer"
+              ? "bg-cyan-700 text-white"
+              : "bg-gray-700 text-gray-300 hover:bg-gray-600"
+          }`}
+          onClick={() => setView("customer")}
+        >
+          Customer View
+        </button>
+        <button
+          className={`px-4 py-2 rounded-md font-semibold ${
+            view === "employee"
+              ? "bg-cyan-700 text-white"
+              : "bg-gray-700 text-gray-300 hover:bg-gray-600"
+          }`}
+          onClick={() => setView("employee")}
+        >
+          Employee View
+        </button>
+      </div>
     </header>
   );
 };
