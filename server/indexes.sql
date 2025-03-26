7cb347c754cbd18393358a93f9da0d11dc876a3b
jwt2706 <jwt2home@gmail.com>
2025-03-26 17:47:09 -0400

    rename idx_hotels

--- server/indexes.sql
+++ server/indexes.sql
@@ -5,7 +5,7 @@ ON RoomAvailableDates (room_available_date, hotel_id, room_num);
 
 DROP INDEX IF EXISTS idx_hotels;
 
-CREATE INDEX idx_rooms 
+CREATE INDEX idx_hotels
 ON Hotels (id, num_rooms, category, hotel_name);
 
 
